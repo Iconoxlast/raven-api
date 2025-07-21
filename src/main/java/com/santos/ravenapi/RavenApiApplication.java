270f8e526e85534b099f44c81b57c5540d86e248
Iconoxlast <santos.j.paulo@hotmail.com>
2025-07-21 14:13:36 -0300

    RavenApi

---
+++ src/main/java/com/santos/ravenapi/RavenApiApplication.java
@@ -0,0 +1,13 @@
+package com.santos.ravenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RavenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RavenApiApplication.class, args);
+	}
+
+}
