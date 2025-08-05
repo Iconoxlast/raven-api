560f25d4474fc92e885c8e90a87714168e75f120
Iconoxlast <santos.j.paulo@hotmail.com>
2025-08-05 18:16:03 -0300

    build: add flyway migration file
    A database will be used to log client requests and persist data regarding characters, appearances and issue details. Still working on the details about the data persistence overall structure; JPA entities and repositories to be created soon

---
+++ src/main/java/com/santos/ravenapi/infra/config/WebConfig.java
@@ -0,0 +1,12 @@
+package com.santos.ravenapi.infra.config;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+	
+	public void addInterceptors(InterceptorRegistry registry) {
+		
+	}
+
+}
---
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -0,0 +1,61 @@
+create table request_log(
+
+    reqs_id bigint not null auto_increment PRIMARY KEY,
+    reqs_method varchar(10) not null,
+    reqs_uri varchar(200) not null,
+    reqs_client_ip varchar(100) not null,
+    reqs_status int not null,
+    reqs_user_agent varchar(200) not null,
+    reqs_locale varchar(50) not null,
+    reqs_response_time int not null,
+    reqs_timestamp datetime not null
+
+);
+create table publishers(
+
+    publ_id bigint not null auto_increment PRIMARY KEY,
+    publ_name varchar(20) not null
+
+);
+create table characters(
+
+    char_id bigint not null auto_increment PRIMARY KEY,
+    char_publ_id bigint not null,
+    char_page_name varchar(100) not null,
+    char_latest_update datetime not null,
+    FOREIGN KEY (char_publ_id) REFERENCES publishers(publ_id)
+
+);
+create table character_disambiguations(
+
+    char_id bigint not null UNIQUE KEY,
+    cver_id bigint not null UNIQUE KEY,
+    FOREIGN KEY (char_id) REFERENCES characters(char_id),
+    FOREIGN KEY (cver_id) REFERENCES character_versions(cver_id)
+);
+create table character_versions(
+
+    cver_id bigint not null auto_increment PRIMARY KEY,
+    cver_char_id bigint not null,
+    cver_page_name varchar(100) not null,
+    char_latest_update datetime not null,
+    FOREIGN KEY (cver_char_id) REFERENCES characters(char_id)
+
+);
+create table issues(
+
+    iss_page_id bigint not null PRIMARY KEY,
+    iss_publ_id bigint not null,
+    iss_page_name varchar(100) not null,
+    iss_publication_date date not null, 
+    FOREIGN KEY (iss_publ_id) REFERENCES publishers(publ_id)
+
+);
+create table character_appearances(
+
+    iss_page_id bigint not null UNIQUE KEY,
+    cver_id bigint not null UNIQUE KEY,
+    FOREIGN KEY (iss_page_id) REFERENCES issues(iss_page_id),
+    FOREIGN KEY (cver_id) REFERENCES character_versions(cver_id)
+
+);
