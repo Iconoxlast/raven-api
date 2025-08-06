77e911e73f45620aad02ffa52bdb290124794148
Iconoxlast <santos__jp@hotmail.com>
2025-08-06 17:46:12 -0300

    refactor: add values to enum elements
    The numbers correspond to a publisher's ID in the publishers table

--- src/main/java/com/santos/ravenapi/search/enums/PublisherEnum.java
+++ src/main/java/com/santos/ravenapi/search/enums/PublisherEnum.java
@@ -1,6 +1,20 @@
 package com.santos.ravenapi.search.enums;
 
+/**
+ * Enum that not only lists the publishers available for queries, but their
+ * respective IDs in the database's "publishers" table.
+ */
 public enum PublisherEnum {
 
-	DC, MARVEL
+	DC(1), MARVEL(2);
+
+	private int id;
+
+	PublisherEnum(int id) {
+		this.id = id;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
