e880884318653cee23f0df278238ead6767b9226
Iconoxlast <santos__jp@hotmail.com>
2025-08-07 17:01:45 -0300

    build: add logging mechanism
    Added an interceptor that collects relevant data about the API request and its processing. The data should eventually be persisted in the database, but that will be implemented later on.

--- src/main/java/com/santos/ravenapi/infra/config/WebConfig.java
+++ src/main/java/com/santos/ravenapi/infra/config/WebConfig.java
@@ -1,12 +1,20 @@
 package com.santos.ravenapi.infra.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.santos.ravenapi.infra.interceptor.LoggingInterceptor;
+
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 	
+	@Autowired
+	private LoggingInterceptor ipInterceptor;
+	
 	public void addInterceptors(InterceptorRegistry registry) {
-		
+		registry.addInterceptor(ipInterceptor).addPathPatterns("**");
 	}
 
 }
