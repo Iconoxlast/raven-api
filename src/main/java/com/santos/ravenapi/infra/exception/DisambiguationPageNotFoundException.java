f48e1748e9ebd8fa788694a4135e8dcd450dc1db
Iconoxlast <santos.j.paulo@hotmail.com>
2025-08-08 19:10:49 -0300

    build: new exceptions for the search services
    They play a part in the logic regarding whether the searched term should yield character appearances or a character disambiguation list

---
+++ src/main/java/com/santos/ravenapi/infra/exception/DisambiguationPageNotFoundException.java
@@ -0,0 +1,6 @@
+package com.santos.ravenapi.infra.exception;
+
+public class DisambiguationPageNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 7312269814542410181L;
+
+}
