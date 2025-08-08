d7827e15e50a83ab36c3766b0bf4860c72b22458
Iconoxlast <santos.j.paulo@hotmail.com>
2025-08-08 19:05:17 -0300

    build: apply the character appearances query logic
    The external API search for character issues and its processing is functional, particularly for DC comics characters. However, the PublisherSearchService interface will most likely be turned into an abstract class. Also changed the signatures of a number of methods so that they'll receive a PublisherEnum as an argument rather than PublisherEndpointEnum

--- src/main/java/com/santos/ravenapi/search/service/FandomQueryService.java
+++ src/main/java/com/santos/ravenapi/search/service/FandomQueryService.java
@@ -2,11 +2,20 @@ package com.santos.ravenapi.search.service;
 
 import java.util.List;
 
-import com.santos.ravenapi.model.vo.IssueDetailsVO;
-import com.santos.ravenapi.search.enums.PublisherEndpointEnum;
+import com.santos.ravenapi.model.dto.search.output.IssueOutput;
+import com.santos.ravenapi.search.enums.PublisherEnum;
 
 public interface FandomQueryService {
 
-	List<IssueDetailsVO> getAppearances(PublisherEndpointEnum publisher, String character);
+	/**
+	 * Requests a list of a particular character's appearances and each appearance's
+	 * publication date through the FandomApiClient, and returns a list of DTOs
+	 * corresponding to the listed issues.
+	 * 
+	 * @param publisher
+	 * @param character
+	 * @return List<IssueOutput>
+	 */
+	List<IssueOutput> getAppearances(PublisherEnum publisher, String character);
 
 }
--- src/main/java/com/santos/ravenapi/search/service/FandomQueryServiceImpl.java
+++ src/main/java/com/santos/ravenapi/search/service/FandomQueryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.santos.ravenapi.search.service;
 
+import java.time.YearMonth;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,9 +13,9 @@ import com.santos.ravenapi.model.dto.search.appearances.CategoryMember;
 import com.santos.ravenapi.model.dto.search.appearances.FandomAppearancesDTO;
 import com.santos.ravenapi.model.dto.search.issues.Category;
 import com.santos.ravenapi.model.dto.search.issues.FandomIssueDetailsDTO;
-import com.santos.ravenapi.model.vo.IssueDetailsVO;
+import com.santos.ravenapi.model.dto.search.output.IssueOutput;
 import com.santos.ravenapi.search.client.FandomApiClient;
-import com.santos.ravenapi.search.enums.PublisherEndpointEnum;
+import com.santos.ravenapi.search.enums.PublisherEnum;
 import com.santos.ravenapi.search.service.parser.FandomDataParser;
 
 @Service
@@ -22,27 +24,30 @@ public class FandomQueryServiceImpl implements FandomQueryService {
 	@Autowired
 	private FandomApiClient apiClient;
 
-	public List<IssueDetailsVO> getAppearances(PublisherEndpointEnum endpoint, String character) {
-		List<IssueDetailsVO> appearancesList = new ArrayList<>();
-		FandomAppearancesDTO appearancesDto = apiClient.queryAppearances(endpoint, character);
-		addAppearancesToList(appearancesList, appearancesDto.query().categorymembers());
+	public List<IssueOutput> getAppearances(PublisherEnum publisher, String character) {
+		List<IssueOutput> appearancesList = new ArrayList<>();
+		FandomAppearancesDTO appearancesDto = apiClient.queryAppearances(publisher.getEndpoint(), character);
+		addAppearancesToList(publisher, appearancesList, appearancesDto.query().categorymembers());
 		if (appearancesDto.cont() != null) {
 			do {
-				appearancesDto = apiClient.queryAppearances(endpoint, character, appearancesDto.cont().cmcontinue());
-				addAppearancesToList(appearancesList, appearancesDto.query().categorymembers());
+				appearancesDto = apiClient.queryAppearances(publisher.getEndpoint(), character,
+						appearancesDto.cont().cmcontinue());
+				addAppearancesToList(publisher, appearancesList, appearancesDto.query().categorymembers());
 			} while (appearancesDto.cont() != null);
 		}
-		addPublicationDates(endpoint, appearancesList);
+		sortAppearancesByPublicationDate(appearancesList);
 		return appearancesList;
 	}
 
-	private void addAppearancesToList(List<IssueDetailsVO> appearancesList, List<CategoryMember> categoryMembers) {
+	private void addAppearancesToList(PublisherEnum endpoint, List<IssueOutput> appearancesList,
+			List<CategoryMember> categoryMembers) {
 		categoryMembers.forEach(categoryMember -> appearancesList
-				.add(new IssueDetailsVO(categoryMember.title(), categoryMember.pageid())));
+				.add(new IssueOutput(getPublicationDate(endpoint, categoryMember.pageid()), categoryMember.title(),
+						categoryMember.pageid())));
 	}
 
 	/**
-	 * Assigns a publication date to each issue by querying issue details through an
+	 * Assigns a publication date to an issue by querying issue details through an
 	 * API request. Generally an issue's article on a comic book fandom has the
 	 * issue's publication date as one of the article's categories, so the most
 	 * reliable way to get the publication date is by sifting through the
@@ -52,16 +57,18 @@ public class FandomQueryServiceImpl implements FandomQueryService {
 	 * articles may not have a publication month or year listed, and in that case,
 	 * the YearMonth "1900-01" is assigned to them for later treatment.
 	 * 
-	 * @param appearancesList
+	 * @param publisher
+	 * @param pageId
 	 */
-	private void addPublicationDates(PublisherEndpointEnum endpoint, List<IssueDetailsVO> appearancesList) {
-		appearancesList.forEach(issue -> {
-			FandomIssueDetailsDTO issueDto = apiClient.queryIssueDetails(endpoint, issue.getId());
-			Optional<Category> optCategory = issueDto.parse().categories().stream()
-					.filter(Category::isCategoryIssueDate)
-					.sorted((cat1, cat2) -> Integer.compare(cat2.category().length(), cat1.category().length()))
-					.findFirst();
-			issue.setDate(FandomDataParser.getYearMonth(optCategory.isPresent() ? optCategory.get().category() : ""));
-		});
+	private YearMonth getPublicationDate(PublisherEnum publisher, int pageId) {
+		FandomIssueDetailsDTO issueDto = apiClient.queryIssueDetails(publisher.getEndpoint(), pageId);
+		Optional<Category> optCategory = issueDto.parse().categories().stream().filter(Category::isCategoryIssueDate)
+				.sorted((cat1, cat2) -> Integer.compare(cat2.category().length(), cat1.category().length()))
+				.findFirst();
+		return FandomDataParser.getYearMonth(optCategory.isPresent() ? optCategory.get().category() : "");
+	}
+
+	private void sortAppearancesByPublicationDate(List<IssueOutput> appearancesList) {
+		appearancesList.sort(Comparator.comparing(IssueOutput::date));
 	}
 }
