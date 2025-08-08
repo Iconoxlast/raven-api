2bb7bda371bbfc6c71ea970bee44ef19b5a8affc
Iconoxlast <santos__jp@hotmail.com>
2025-08-08 13:05:14 -0300

    fix: remove String.format calls
    Replaced all the String.format() uses with StringBuilders to make sure IllegalFormatConversionException is avoided.

--- src/main/java/com/santos/ravenapi/search/util/QueryFormatter.java
+++ src/main/java/com/santos/ravenapi/search/util/QueryFormatter.java
@@ -7,23 +7,25 @@ public class QueryFormatter {
 	public static int BUFFER = 500;
 
 	public synchronized static String characterAppearances(PublisherEndpointEnum publisher, String character) {
-		return String.format(
-				"%s?action=query&format=json&formatversion=2&list=categorymembers&cmlimit=%d&cmtitle=Category%3A%s/Appearances",
-				publisher.getUrl(), BUFFER, character);
+		return new StringBuilder(publisher.getUrl())
+				.append("?action=query&format=json&formatversion=2&list=categorymembers&cmlimit=").append(BUFFER)
+				.append("&cmtitle=Category%3A").append(character).append("/Appearances").toString();
 	}
 
 	public synchronized static String characterAppearancesPagination(PublisherEndpointEnum publisher, String character,
 			String nextPageCode) {
-		return new StringBuilder(characterAppearances(publisher, character))
-				.append(String.format("&continue=-||&cmcontinue=%s", nextPageCode)).toString();
+		return new StringBuilder(characterAppearances(publisher, character)).append("&continue=-||&cmcontinue=")
+				.append(nextPageCode).toString();
 	}
 
 	public synchronized static String issueDetails(PublisherEndpointEnum publisher, String issueId) {
-		return String.format("%s?action=parse&format=json&formatversion=2&pageid=%s", publisher.getUrl(), issueId);
+		return new StringBuilder(publisher.getUrl()).append("?action=parse&format=json&formatversion=2&pageid=")
+				.append(issueId).toString();
 	}
 
 	public synchronized static String disambiguationPage(PublisherEndpointEnum publisher, String characterAlias) {
-		return String.format("%s?action=query&prop=revisions&rvprop=content&format=json&titles=%s", publisher.getUrl(),
-				characterAlias);
+		return new StringBuilder(publisher.getUrl())
+				.append("?action=query&prop=revisions&rvprop=content&format=json&titles=").append(characterAlias)
+				.toString();
 	}
 }
