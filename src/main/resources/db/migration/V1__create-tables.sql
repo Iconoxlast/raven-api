3a3c8698467b7bf805a7095cd79248d9dc1979fd
Iconoxlast <santos.j.paulo@hotmail.com>
2025-08-11 15:46:36 -0300

    fix: create table script
    Added a unique key to the tables 'characters' and 'character_versions' in order to ensure there won't be repeated combinations of publisher IDs and page names on insert operations. Also added 'ON DELETE' constraint to the join tables, ensuring data consistency in case any referenced record in a parent table is deleted

--- src/main/resources/db/migration/V1__create-tables.sql
+++ src/main/resources/db/migration/V1__create-tables.sql
@@ -1,5 +1,4 @@
 create table request_log(
-
     reqs_id bigint not null auto_increment PRIMARY KEY,
     reqs_method varchar(10) not null,
     reqs_uri varchar(200) not null,
@@ -9,54 +8,47 @@ create table request_log(
     reqs_locale varchar(50) not null,
     reqs_response_time int not null,
     reqs_timestamp datetime not null
-
 );
 create table publishers(
-
     publ_id bigint not null auto_increment PRIMARY KEY,
     publ_name varchar(20) not null
-
 );
 INSERT INTO publishers (publ_name) VALUES ('DC'),('Marvel');
 create table characters(
-
     char_id bigint not null auto_increment PRIMARY KEY,
     char_publ_id bigint not null,
-    char_page_name varchar(100) not null UNIQUE,
+    char_page_name varchar(100) not null,
     char_latest_update datetime not null,
+    UNIQUE KEY cver_uq (char_publ_id, char_page_name),
     FOREIGN KEY (char_publ_id) REFERENCES publishers(publ_id)
-
 );
 create table character_versions(
-
     cver_id bigint not null auto_increment PRIMARY KEY,
     cver_publ_id bigint not null,
-    cver_page_name varchar(100) not null UNIQUE,
+    cver_page_name varchar(100) not null,
     cver_latest_update datetime not null,
+    UNIQUE KEY cver_uq (cver_publ_id, cver_page_name),
     FOREIGN KEY (cver_publ_id) REFERENCES publishers(publ_id)
 
 );
 create table character_disambiguations(
-
     char_id bigint not null,
     cver_id bigint not null,
-    FOREIGN KEY (char_id) REFERENCES characters(char_id),
-    FOREIGN KEY (cver_id) REFERENCES character_versions(cver_id)
+    PRIMARY KEY (char_id, cver_id),
+    FOREIGN KEY (char_id) REFERENCES characters(char_id) ON DELETE cascade,
+    FOREIGN KEY (cver_id) REFERENCES character_versions(cver_id) ON DELETE cascade
 );
 create table issues(
-
     iss_page_id bigint not null PRIMARY KEY,
     iss_publ_id bigint not null,
     iss_page_name varchar(100) not null UNIQUE,
     iss_publication_date date not null, 
     FOREIGN KEY (iss_publ_id) REFERENCES publishers(publ_id)
-
 );
 create table character_appearances(
-
     iss_page_id bigint not null,
     cver_id bigint not null,
-    FOREIGN KEY (iss_page_id) REFERENCES issues(iss_page_id),
-    FOREIGN KEY (cver_id) REFERENCES character_versions(cver_id)
-
+    PRIMARY KEY (iss_page_id, cver_id),
+    FOREIGN KEY (iss_page_id) REFERENCES issues(iss_page_id) ON DELETE cascade,
+    FOREIGN KEY (cver_id) REFERENCES character_versions(cver_id) ON DELETE cascade
 );
