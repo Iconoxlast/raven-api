47f4d3dca6745d13ac1c5c7812e8b808096c9563
Iconoxlast <santos.j.paulo@hotmail.com>
2025-08-13 15:43:16 -0300

    build: add ResponseBodyBuilder
    Utility class to build a JSON body for the exceptions being treated by the ExceptionHandlers class

---
+++ src/main/java/com/santos/ravenapi/search/util/ResponseBodyBuilder.java
@@ -0,0 +1,16 @@
+package com.santos.ravenapi.search.util;
+
+import java.time.LocalDateTime;
+
+public class ResponseBodyBuilder {
+
+	public synchronized static String error(int status, String error, String message) {
+		return String.format("""
+				{
+					"timestamp": %s,
+					"status": %d,
+					"error": "%s",
+					"message": "%s"
+				}""", LocalDateTime.now().toString(), status, error, message);
+	}
+}
