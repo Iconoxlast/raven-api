92b84ef46317460c46fd707b4957d725c391a205
Iconoxlast <santos.j.paulo@hotmail.com>
2025-08-14 19:52:19 -0300

    feat: tests
    More tests will be added. I lost the ones I had with a restore command

--- src/test/java/com/santos/ravenapi/RavenapiApplicationTests.java
+++ src/test/java/com/santos/ravenapi/RavenapiApplicationTests.java
@@ -1,13 +1,30 @@
 package com.santos.ravenapi;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.santos.ravenapi.model.dto.output.OutputDTO;
+import com.santos.ravenapi.search.enums.PublisherEnum;
+import com.santos.ravenapi.search.service.SearchService;
+
 @SpringBootTest
-class RavenapiApplicationTests {
+class RavenApiApplicationTests {
+
+	@Autowired
+	private SearchService service;
 
 	@Test
-	void contextLoads() {
+	void returnsCharacterNotFoundException() {
+		OutputDTO output = null;
+		try {
+			output = service.getCharacterData(PublisherEnum.DC, "asdasda");			
+		} catch (Exception e) {
+			System.out.println(e.getClass());
+		}
+		assertThat(output).isNull();
 	}
 
 }
