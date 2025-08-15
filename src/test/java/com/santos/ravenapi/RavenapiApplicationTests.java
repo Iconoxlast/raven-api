fafccd4a400cd4d6bf5cad09174e223a365cdede
Iconoxlast <santos.j.paulo@hotmail.com>
2025-08-15 17:52:21 -0300

    test: character appearances fandom API unit tests
    A few tests made in order to evaluate the changes to the querying procedures and their performance

--- src/test/java/com/santos/ravenapi/RavenapiApplicationTests.java
+++ src/test/java/com/santos/ravenapi/RavenapiApplicationTests.java
@@ -2,20 +2,30 @@ package com.santos.ravenapi;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+import com.santos.ravenapi.model.dto.output.IssueOutput;
 import com.santos.ravenapi.model.dto.output.OutputDTO;
 import com.santos.ravenapi.search.enums.PublisherEnum;
+import com.santos.ravenapi.search.service.FandomQueryService;
 import com.santos.ravenapi.search.service.SearchService;
 
 @SpringBootTest
+@ActiveProfiles("debug")
 class RavenApiApplicationTests {
 
 	@Autowired
 	private SearchService service;
-
+	@Autowired
+	private FandomQueryService fandomQueryService;
+	
 	@Test
 	void returnsCharacterNotFoundException() {
 		OutputDTO output = null;
@@ -26,5 +36,41 @@ class RavenApiApplicationTests {
 		}
 		assertThat(output).isNull();
 	}
+	
+	@Test
+	void getsCalculatorAppearances() {
+		Optional<List<IssueOutput>> appearances = Optional.empty();
+		try {
+			appearances = getCharacterAppearances("Noah Kuttler (Prime Earth)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertThat(appearances).isNotEmpty();
+		assertThat(appearances.get()).isNotEmpty();
+	}
 
+	@Test
+	void getsRavenAppearances() {
+		Optional<List<IssueOutput>> appearances = Optional.empty();
+		try {
+			appearances = getCharacterAppearances("Raven (Prime Earth)");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		assertThat(appearances).isNotEmpty();
+		assertThat(appearances.get()).isNotEmpty();
+	}
+	
+	private Optional<List<IssueOutput>> getCharacterAppearances(String character) {
+		long startTime = Instant.now().toEpochMilli();
+		Optional<List<IssueOutput>> appearances = Optional.empty();
+		try {
+			appearances = fandomQueryService.getAppearances(PublisherEnum.DC, character);
+			appearances.get().forEach(issue -> System.out.printf("%s - %s\r\n", issue.date(), issue.title()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.err.printf("%.4f s\r\n", (double) (Instant.now().toEpochMilli() - startTime) / 1000);
+		return appearances;
+	}
 }
