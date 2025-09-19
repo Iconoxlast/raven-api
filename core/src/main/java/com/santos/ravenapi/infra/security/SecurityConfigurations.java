354b1d2ce15ec1a8375e4fec970e1cc771319cb7
Iconoxlast <santos.j.paulo@hotmail.com>
2025-09-19 16:56:40 -0300

    fix: add security configurations
    It was necessary to add explicit authorizations in order for the front end's requests not to be blocked by CORS policy

---
+++ core/src/main/java/com/santos/ravenapi/infra/security/SecurityConfigurations.java
@@ -0,0 +1,38 @@
+package com.santos.ravenapi.infra.security;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf(csrf -> csrf.disable())
+				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.cors(Customizer.withDefaults())
+				.authorizeHttpRequests(
+						request -> request.requestMatchers("/search").permitAll().anyRequest().authenticated())
+				.build();
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOriginPatterns(List.of("http://127.0.0.1:*", "http://localhost:*"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+}
